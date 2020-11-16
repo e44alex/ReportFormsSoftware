7dfadeafbc3e0ff2ab6ed2500174e7189b6f365d
Александр Петроченко <e44alex@gmail.com>
2020-11-16 11:38:12 +0300

    views prototype

--- Diplom/Shared/Model/ReportForm.cs
+++ Diplom/Shared/Model/ReportForm.cs
@@ -58,27 +58,27 @@ namespace Diplom.Shared.Model
         //fields
         public string RowCode { get; set; }
 
-        public decimal C1 { get; set; }
-        public decimal C2 { get; set; }
-        public decimal C3 { get; set; }
-        public decimal C4 { get; set; }
-        public decimal C5 { get; set; }
-        public decimal C6 { get; set; }
-        public decimal C7 { get; set; }
-        public decimal C8 { get; set; }
-        public decimal C9 { get; set; }
-        public decimal C10 { get; set; }
-        public decimal C11 { get; set; }
-        public decimal C12 { get; set; }
-        public decimal C13 { get; set; }
-        public decimal C14 { get; set; }
-        public decimal C15 { get; set; }
-        public decimal C16 { get; set; }
-        public decimal C17 { get; set; }
-        public decimal C18 { get; set; }
-        public decimal C19 { get; set; }
-        public decimal C20 { get; set; }
-        public decimal C21 { get; set; }
-        public decimal C22 { get; set; }
+        public string C1 { get; set; }
+        public string C2 { get; set; }
+        public string C3 { get; set; }
+        public string C4 { get; set; }
+        public string C5 { get; set; }
+        public string C6 { get; set; }
+        public string C7 { get; set; }
+        public string C8 { get; set; }
+        public string C9 { get; set; }
+        public string C10 { get; set; }
+        public string C11 { get; set; }
+        public string C12 { get; set; }
+        public string C13 { get; set; }
+        public string C14 { get; set; }
+        public string C15 { get; set; }
+        public string C16 { get; set; }
+        public string C17 { get; set; }
+        public string C18 { get; set; }
+        public string C19 { get; set; }
+        public string C20 { get; set; }
+        public string C21 { get; set; }
+        public string C22 { get; set; }
     }
 }
