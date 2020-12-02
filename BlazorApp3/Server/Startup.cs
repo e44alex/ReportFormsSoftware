c99d9b2c1774da8d0ee4814aee3397bcbfb6b024
Александр Петроченко <77230061@study.bsuir.by>
2020-12-03 00:39:51 +0300

    Добавьте файлы проекта.

---
+++ BlazorApp3/Server/Startup.cs
@@ -0,0 +1,79 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+using Diplom.Shared.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddControllersWithViews();
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddHttpContextAccessor();
+            services.AddScoped<HttpContextAccessor>();
+            services.AddHttpClient();
+            services.AddScoped<HttpClient>();
+            services.AddSession();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                
+                endpoints.MapFallbackToFile("index.html");
+            });
+        }
+    }
+}
---
+++ BlazorApp3/Shared/Model/ReportForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diplom.Shared.Model
+{
+    public enum FormType
+    {
+       PERIODIC,
+       NON_PERIODIC
+    }
+
+    public enum PeriodicFormType
+    {
+        MONTH,
+        QUARTER,
+        HALF_YEAR,
+        YEAR
+    }
+
+    public enum FormState
+    {
+        CLEAR,
+        IN_WORK,
+        FILLED,
+        CHECKED,
+        SUBMITTED
+    }
+
+    public class ReportForm
+    {
+        public Guid FormHeadId { get; set; }
+        public Guid FormDataId{ get; set; }
+
+        public ReportFormHead FormHead { get; set; }
+        public ReportFormData FormData { get; set; }
+        public FormState State{ get; set; }
+    }
+
+    public class ReportFormHead
+    {
+        public Guid Id { get; set; }
+        public int Cls { get; set; } // классификатор
+        public int Code { get; set; }//код
+        public int Chapter { get; set; }//глава
+        public int Paragraph { get; set; }//параграф
+        public int SubPragraph { get; set; }//ПодПараграф
+        public int Program { get; set; }//Программа
+        public int SubProgram { get; set; }//Под Программа
+        public FormType Type { get; set; }
+        public PeriodicFormType PeriodicType { get; set; }
+        public int OrganisationId { get; set; }
+        public Organisation Organisation { get; set; }
+        public ReportPeriod ReportPeriod { get; set; }
+    }
+
+    public class ReportFormData
+    {
+        //PrimaryKey
+        public Guid FormHeadId { get; set; }
+        public ReportFormHead FormHead { get; set; }
+        public Guid ReportPeriodId { get; set; }
+        public ReportPeriod ReportPeriod { get; set; }
+
+        //fields
+        public string RowCode { get; set; }
+
+        public string C1 { get; set; }
+        public string C2 { get; set; }
+        public string C3 { get; set; }
+        public string C4 { get; set; }
+        public string C5 { get; set; }
+        public string C6 { get; set; }
+        public string C7 { get; set; }
+        public string C8 { get; set; }
+        public string C9 { get; set; }
+        public string C10 { get; set; }
+        public string C11 { get; set; }
+        public string C12 { get; set; }
+        public string C13 { get; set; }
+        public string C14 { get; set; }
+        public string C15 { get; set; }
+        public string C16 { get; set; }
+        public string C17 { get; set; }
+        public string C18 { get; set; }
+        public string C19 { get; set; }
+        public string C20 { get; set; }
+        public string C21 { get; set; }
+        public string C22 { get; set; }
+    }
+}
